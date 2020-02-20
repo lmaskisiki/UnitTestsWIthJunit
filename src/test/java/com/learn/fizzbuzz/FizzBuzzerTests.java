ed40557e532b3586d652e25ff3a047a3c6dc6432
Lizo Masikisiki <lizo@s-squared.co.za>
2020-02-20 06:38:20 +0200

    commit expect the unexpected

--- src/test/java/com/learn/fizzbuzz/FizzBuzzerTests.java
+++ src/test/java/com/learn/fizzbuzz/FizzBuzzerTests.java
@@ -1,7 +1,11 @@
 package com.learn.fizzbuzz;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class FizzBuzzerTests {
 
@@ -35,7 +39,6 @@ public class FizzBuzzerTests {
         Assert.assertEquals("4", results);
     }
 
-
     @Test
     public void fizzBuzz_givenSeven_ShouldReturnSeven() {
         //arrange
@@ -54,6 +57,47 @@ public class FizzBuzzerTests {
         String results = sut.fizzBuzz(8);
         //assert
         Assert.assertEquals("8", results);
+    }
+
+    @Test
+    public void fizzBuzz_givenNegativeNumbers_ThrowNegativeNumbersNotAllowedException_1() {
+        //arrange
+        FizzBuzzer sut = new FizzBuzzer();
+        //action
+        Exception exception = assertThrows(NegativeNumbersNowAllowedException.class, () ->
+                sut.fizzBuzz(-5));
+        assertEquals("Negative numbers are not supported", exception.getMessage());
+    }
+
+    @Test
+    public void fizzBuzz_givenNegativeNumbers_ThrowNegativeNumbersNotAllowedException_2() {
+        //arrange
+        FizzBuzzer sut = new FizzBuzzer();
+        //action
+        Exception exception = assertThrows(NegativeNumbersNowAllowedException.class, () ->
+                sut.fizzBuzz(-10));
+        assertEquals("Negative numbers are not supported", exception.getMessage());
+    }
+
+    @Test
+    public void fizzBuzz_givenNegativeNumbers_ThrowNegativeNumbersNotAllowedException_3() {
+        //arrange
+        FizzBuzzer sut = new FizzBuzzer();
+        //action
+        Exception exception = assertThrows(NegativeNumbersNowAllowedException.class, () ->
+                sut.fizzBuzz(-1000));
+        assertEquals("Negative numbers are not supported", exception.getMessage());
+    }
+
+     //DEVENOTE: Junit @Rule, @Test(expected =  Exception.class)
+    @Test
+    public void fizzBuzz_givenNegativeNumbers_ThrowNegativeNumbersNotAllowedException_5() {
+        //arrange
+        FizzBuzzer sut = new FizzBuzzer();
+        //action
+        Assertions.assertThatThrownBy(() -> sut.fizzBuzz(-10000))
+                .isInstanceOf(NegativeNumbersNowAllowedException.class)
+                .hasMessage("Negative numbers are not supported");
     }
 }
 
